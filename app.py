137aa5d014db9f9fe3b74c3a604f7b1a87fb634f
Alican Akca <alican.akca@std.ieu.edu.tr>
2022-08-15 14:52:35 +0300

    Model has been changed because of the licence.

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 import cv2
+import torch
 import numpy as np
 import gradio as gr
 import paddlehub as hub
+from PIL import Image
 from methods.img2pixl import pixL
 from examples.pixelArt.combine import combine
-from examples.pixelArt.white_box_cartoonizer.cartoonize import WB_Cartoonize
+
 model = hub.Module(name='U2Net')
-pixl = pixL()
-combine = combine()
+device = "cuda" if torch.cuda.is_available() else "cpu"
+face2paint = torch.hub.load("bryandlee/animegan2-pytorch:main", "face2paint", device=device, size=512)
+model = torch.hub.load("bryandlee/animegan2-pytorch", "generator", device=device).eval()
 
 def initilize(image,pixel_size, checkbox1):
-    image = cv2.imread(image.name)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = WB_Cartoonize().infer(image)
-    image = np.array(image)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    if checkbox1:
+    image = Image.open(image.name).convert("RGB")
+    image = cv2.cvtColor(np.asarray(face2paint(model, image)), cv2.COLOR_BGR2RGB)
+
+    if checkbox1:#With the Object Oriented Inference
       result = model.Segmentation(
           images=[image],
           paths=None,
@@ -23,11 +24,11 @@ def initilize(image,pixel_size, checkbox1):
           input_size=320,  
           output_dir='output',
           visualization=True)
-      result = combine.combiner(images = pixl.toThePixL([result[0]['front'][:,:,::-1], result[0]['mask']], 
+      result = combine().combiner(images = pixL().toThePixL([result[0]['front'][:,:,::-1], result[0]['mask']], 
                                                         pixel_size),
                                 background_image = image)
-    else:
-      result = pixl.toThePixL([image], pixel_size)
+    else: #Without the Object Oriented Inference
+      result = pixL().toThePixL([image], pixel_size)
     return result
 
 inputs = [gr.inputs.Image(type='file', label="Image"),
@@ -38,5 +39,6 @@ outputs = [gr.Image(type="file",label="Front")]
 description = "Pixera for GIF and Video will be released soon."
 gr.Interface(fn = initilize,
                     inputs = inputs,
-                    outputs = outputs).launch()
+                    outputs = outputs,
+                    description=description).launch()
 
